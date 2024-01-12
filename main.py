3815f3c55554ed6f87c60a7daf1dba34fca283dd
park102 <BJohn316@outlook.com>
2024-01-11 19:55:45 -0500

    start of rewrite

--- main.py
+++
@@ -1,5 +0,0 @@
-import intro, menu
-
-start = intro.Start()
-start.is_new()
-menu.run()
--- rooms.py
+++
@@ -1,9 +0,0 @@
-try:
-    import Rooms.room2
-    room = Rooms.room2
-except:
-    try:
-        import Rooms.room1
-        room = Rooms.room1
-    except:
-        pass
